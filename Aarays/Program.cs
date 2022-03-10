2bce3b07ec23306b8d30169e563bfeb0416e1496
Cedric Wiest <cedric.w7@gso.schule.koeln>
2022-03-10 09:59:01 +0100

    Projektdateien hinzufügen.

---
+++ Aarays/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Bitte gebe die noten in der Reinfolge ein");
+int i = 20;
+    int[] Noten = new int[i];
+
+    Noten[0] = Convert.ToInt16(Console.ReadLine());
+    Noten[1] = Convert.ToInt16(Console.ReadLine());
+    Noten[2] = Convert.ToInt16(Console.ReadLine());
+    Noten[3] = Convert.ToInt16(Console.ReadLine());
+    Noten[4] = Convert.ToInt16(Console.ReadLine());
+    Noten[5] = Convert.ToInt16(Console.ReadLine());
+    Noten[6] = Convert.ToInt16(Console.ReadLine());
+    Noten[7] = Convert.ToInt16(Console.ReadLine());
+    Noten[8] = Convert.ToInt16(Console.ReadLine());
+    Noten[9] = Convert.ToInt16(Console.ReadLine());
+    Noten[10] = Convert.ToInt16(Console.ReadLine());
+    Noten[12] = Convert.ToInt16(Console.ReadLine());
+    Noten[13] = Convert.ToInt16(Console.ReadLine());
+    Noten[14] = Convert.ToInt16(Console.ReadLine());
+    Noten[15] = Convert.ToInt16(Console.ReadLine());
+    Noten[16] = Convert.ToInt16(Console.ReadLine());
+    Noten[17] = Convert.ToInt16(Console.ReadLine());
+    Noten[18] = Convert.ToInt16(Console.ReadLine());
+    Noten[19] = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("--------------------------------------");
+
+for(int i = 0; i < Noten.Length; i++)
+{
+    Console.WriteLine(Noten[i]);
+
+}
+
+
+
+
+
+
+
+
+
+
